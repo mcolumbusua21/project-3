ee5cb118ad3b4bd76ec709363018b86d3652193d
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2021-05-04 14:04:13 -0700

    setup yelp endpoint

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
-const axios = require('axios')
+const axios = require("axios");
 
 const mongoose = require("mongoose");
-const routes = require("./controllers");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -16,22 +17,23 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(express.static("public"));
 // Add routes, both API and view
-app.get('/dispensarylist', async(req,res) => {
+app.get("/dispensarylist", async (req, res) => {
   try {
-    const response = await axios.get(`https://api.yelp.com/v3/businesses/search?categories=dispensary,dispensaries`)
-    const { data } = response
-    res.json(data)
-    
-  } catch(err) {
-    console.log(err)
+    const response = await axios.get(
+      `https://api.yelp.com/v3/businesses/search?categories=dispensary,dispensaries`
+    );
+    const { data } = response;
+    res.json(data);
+  } catch (err) {
+    console.log(err);
   }
-})
+});
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
