d339d24b893c010c8139dbc844bcfcbef489f1f4
Mark Columbus <mcolumbusua21@gmail.com>
2021-05-05 16:29:47 -0700

    why

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,7 +8,7 @@ import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./ForgotPassword";
 import UpdateProfile from "./UpdateProfile";
-import SearchResultsPage from "../pages/SearchResultsPage"
+// import SearchResultsPage from "../pages/SearchResultsPage"
 import ProfilePage from "../pages/ProfilePage"
 import SingleSearchPage from "../pages/SingleSearchPage"
 import FavoritesPage from "../pages/FavoritesPage"
@@ -28,7 +28,7 @@ function App() {
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
               <Route path="/forgot-password" component={ForgotPassword} />
-              <Route path="/search-results" component={SearchResultsPage} />
+              {/* <Route path="/search-results" component={SearchResultsPage} /> */}
               <Route path="/profile" component={ProfilePage} />
               <Route path="/single-search" component={SingleSearchPage} />
               <Route path="/favorites" component={FavoritesPage} />
--- client/src/pages/SearchResultsPage.js
+++ client/src/pages/SearchResultsPage.js
@@ -1,52 +1,5 @@
 // import React, { useState, useEffect } from "react";
 // import { getDispensaries } from "../utils/API";
-<<<<<<< HEAD
-
-function SearchResults(){
-//  const [results, setResults] = useState([])
-//  const [resultObject, setResultObject] = useState({})
-
-//  useEffect(() => {
-//      loadSearchResults()
-//  },[])
-
-//  function loadResults(){
-//      getDispensaries.getSearchResults()
-//      .then(res =>
-//         setSearchResults(res.data)
-//         )
-//         .catch(err => console.log(err));
-//  };
-
-//  function handleInputChange(e){
-//      const { name, value } = e.target;
-//      setResultObject({...resultObject, [name]: value})
-//  };
-
-//  function handleFormSubmit(e){
-//      e.preventDefault();
-//      if(resultObject.businesses){
-//          getDispensaries.saveSearchResults({
-//          name: resultObject.name,
-//          location: resultObject.location.display_location,
-//          phone: resultObject.display_phone,
-//          open: resultObject.is_open,
-//          rating: resultObject.rating,
-//      })
-//      .then(res => loadSearchResults())
-//      .catch(err => console.log(err));
-//     }
-//  };
-
- return(
- <div>
-     <h1>Dispensaries Near Here</h1>
- </div>
- )
-}
-
-export default SearchResults
-=======
 // import { Col, Row, Card, ListGroup } from "react-bootstrap";
 // import Formfiller from "../components/Form/index"
 
@@ -111,4 +64,3 @@ export default SearchResults
 //   );
 // }
 // export default SearchResults;
->>>>>>> c5d9f956d871947320ae2a833f4772e058202561
