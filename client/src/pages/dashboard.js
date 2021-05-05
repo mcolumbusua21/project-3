ef42cfbd9f6beb9ef9df941838e823c9257253da
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-05 15:45:02 -0700

    changes to dashboard and searchresults

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from "react";
-// import { Card, Button, Alert } from "react-bootstrap";
-
-// import { Link, useHistory } from "react-router-dom";
+import { Col, Row, Card, ListGroup } from "react-bootstrap";
+import Formfiller from "../components/Form/index";
 import { getDispensaries } from "../utils/API";
 import Hero from "../components/Hero/Hero";
+// import { Card, Button, Alert } from "react-bootstrap";
+// import { Link, useHistory } from "react-router-dom";
 
 function Dashboard() {
-//   const [error, setError] = useState("");
   const [dispensaries, setDispensaries] = useState([]);
-//   const { currentUser, logout } = useAuth();
-  // const history = useHistory();
 
   useEffect(() => {
     setDispensaries(getDispensaries());
@@ -17,25 +15,75 @@ function Dashboard() {
 
   console.log("DISPENSARY LIST ==> ", dispensaries);
 
-//   async function handleLogout() {
-//     setError("");
+  function SearchResults() {
+  const [results, setResults] = useState([]);
+  const [resultObject, setResultObject] = useState({});
 
-//     try {
-//       await logout();
-//       history.pushState("/login");
-//     } catch {
-//       setError("Failed to log out");
-//     }
-//   }
+  useEffect(() => {
+    loadSearchResults();
+  }, []);
 
-  return (
+  function loadResults() {
+    getDispensaries
+      .getSearchResults()
+      .then((res) => setSearchResults(res.data))
+      .catch((err) => console.log(err));
+  }
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setResultObject({ ...resultObject, [name]: value });
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (resultObject.businesses) {
+      getDispensaries
+        .saveSearchResults({
+          name: resultObject.name,
+          location: resultObject.location.display_location,
+          phone: resultObject.display_phone,
+          open: resultObject.is_open,
+          rating: resultObject.rating,
+        })
+        .then((res) => loadSearchResults())
+        .catch((err) => console.log(err));
+    }
+  }
+  }
+
+
+return (
     <>
       <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg">
         <h1>Celp</h1>
         <h2>It's the Yelp for Cannabis!</h2>
       </Hero>
+     
+    <div>
+      <h1>Dispensaries Near Here</h1>
+      <Row>
+        <Col size="md-6">
+          <ListGroup>
+              {results.map(results => (
+                  <ListItem key={results}></ListItem>
+              ))}
+            <Card>
+              <Card.Body>
+                <Card.Title>{results.name}</Card.Title>
+                <Card.Text>
+                  {results.location} {results.phone} {results.open}{" "}
+                  {results.rating}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </ListGroup>
+        </Col>
+      </Row>
+    </div>
+  
     </>
   );
 }
 
-export default Dashboard;
+export default Dashboard
