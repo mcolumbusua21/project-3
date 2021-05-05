16b7bca8375d834e56cc11a458d9d065e4b814bb
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-05-05 16:28:19 -0700

    clean up

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,56 +1,56 @@
-import React, { useState, useEffect } from "react";
-import { Col, Row, Card, ListGroup } from "react-bootstrap";
-import Formfiller from "../components/Form/index";
-import { getDispensaries } from "../utils/API";
+import React from "react";
+// import { Col, Row, Card, ListGroup } from "react-bootstrap";
+// import Formfiller from "../components/Form/index";
+// import { getDispensaries } from "../utils/API";
 import Hero from "../components/Hero/Hero";
 // import { Card, Button, Alert } from "react-bootstrap";
 // import { Link, useHistory } from "react-router-dom";
 
 function Dashboard() {
-  const [dispensaries, setDispensaries] = useState([]);
+  // const [dispensaries, setDispensaries] = useState([]);
 
-  useEffect(() => {
-    setDispensaries(getDispensaries());
-  }, []);
+  // useEffect(() => {
+  //   setDispensaries(getDispensaries());
+  // }, []);
 
-  console.log("DISPENSARY LIST ==> ", dispensaries);
+  // console.log("DISPENSARY LIST ==> ", dispensaries);
 
-  function SearchResults() {
-  const [results, setResults] = useState([]);
-  const [resultObject, setResultObject] = useState({});
+  // function SearchResults() {
+  // const [results, setResults] = useState([]);
+  // const [resultObject, setResultObject] = useState({});
 
-  useEffect(() => {
-    loadSearchResults();
-  }, []);
+  // useEffect(() => {
+  //   loadSearchResults();
+  // }, []);
 
-  function loadResults() {
-    getDispensaries
-      .getSearchResults()
-      .then((res) => setSearchResults(res.data))
-      .catch((err) => console.log(err));
-  }
+  // function loadResults() {
+  //   getDispensaries
+  //     .getSearchResults()
+  //     .then((res) => setSearchResults(res.data))
+  //     .catch((err) => console.log(err));
+  // }
 
-  function handleInputChange(e) {
-    const { name, value } = e.target;
-    setResultObject({ ...resultObject, [name]: value });
-  }
+  // function handleInputChange(e) {
+  //   const { name, value } = e.target;
+  //   setResultObject({ ...resultObject, [name]: value });
+  // }
 
-  function handleFormSubmit(e) {
-    e.preventDefault();
-    if (resultObject.businesses) {
-      getDispensaries
-        .saveSearchResults({
-          name: resultObject.name,
-          location: resultObject.location.display_location,
-          phone: resultObject.display_phone,
-          open: resultObject.is_open,
-          rating: resultObject.rating,
-        })
-        .then((res) => loadSearchResults())
-        .catch((err) => console.log(err));
-    }
-  }
-  }
+  // function handleFormSubmit(e) {
+  //   e.preventDefault();
+  //   if (resultObject.businesses) {
+  //     getDispensaries
+  //       .saveSearchResults({
+  //         name: resultObject.name,
+  //         location: resultObject.location.display_location,
+  //         phone: resultObject.display_phone,
+  //         open: resultObject.is_open,
+  //         rating: resultObject.rating,
+  //       })
+  //       .then((res) => loadSearchResults())
+  //       .catch((err) => console.log(err));
+  //   }
+  // }
+  // }
 
 
 return (
@@ -62,7 +62,7 @@ return (
      
     <div>
       <h1>Dispensaries Near Here</h1>
-      <Row>
+      {/* <Row>
         <Col size="md-6">
           <ListGroup>
               {results.map(results => (
@@ -79,7 +79,7 @@ return (
             </Card>
           </ListGroup>
         </Col>
-      </Row>
+      </Row> */}
     </div>
   
     </>
