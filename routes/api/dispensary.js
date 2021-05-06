bacc15fab5f196f4e6062ad4766f6153a6eb26bf
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2021-05-06 13:38:44 -0700

    refactor dashboard

--- routes/api/dispensary.js
+++ routes/api/dispensary.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const axios = require("axios");
 router.get("/", async ({ query: { location, limit } }, res) => {
   try {
+    if (!location) throw new Error("location url parameter must be provided");
     const { data } = await axios.get(
       "https://api.yelp.com/v3/businesses/search",
       {
@@ -10,7 +11,7 @@ router.get("/", async ({ query: { location, limit } }, res) => {
           "Content-type": "application/json",
         },
         params: {
-          location: location || "phoenix",
+          location: location,
           term: "dispensary",
           limit: limit || 10,
         },
@@ -18,8 +19,10 @@ router.get("/", async ({ query: { location, limit } }, res) => {
     );
     res.json(data);
   } catch (err) {
-    console.log("ERR", err);
-    res.status(400).json(err);
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
   }
 });
 
