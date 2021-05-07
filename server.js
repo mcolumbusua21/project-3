b275b6b080107169d3e1a881d5feeb9742a78b0c
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-06 20:57:39 -0700

    mongodb?

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-
+// const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/api/dispensary");
 const app = express();
 const PORT = process.env.PORT || 3002;
 
@@ -17,18 +17,27 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(express.static("public"));
 
-app.use(routes);
+//use api routes
+app.use("/api", routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/favorite_db");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/favorite_db", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
 
-
-
+//mongodb URI
+//mongodb+srv://stephanie:8385tas@cluster0.s3qz3.mongodb.net/favorite_db?retryWrites=true&w=majority
 
 
 
