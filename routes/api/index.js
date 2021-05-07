7f60cae4d0aec6b885e7843ae2e420d7999c449e
Mark Columbus <mcolumbusua21@gmail.com>
2021-05-07 10:44:01 -0700

    help

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,5 @@
-// const router = require("express").Router();
 
-// router.use("/dispensary", require("./dispensary"));
-
-// module.exports = router;
 const router = require('express').Router();
-const dispRoutes = require('./dispensary')
-router.use('/dispensary', dispRoutes)
+
+router.use('/dispensary', require('./dispensary'))
 module.exports = router
