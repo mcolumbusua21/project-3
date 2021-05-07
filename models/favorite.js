465bd074bab039b71e145284d0b063b8c90815a4
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-07 11:50:29 -0700

    changes to model, favorite route

--- models/favorite.js
+++ models/favorite.js
@@ -6,6 +6,7 @@ const favoriteSchema = new Schema({
   name: { type: String, required: true },
   display_address: { type: String, required: true },
   display_phone: { type: String, required: true },
+  is_open: { type: Boolean, required: true },
   rating: { type: String, required: true },  
 });
 
