eecce8815f28ec1332f1964bda5fd216381e947a
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-07 17:07:35 -0700

    changed title

--- client/src/components/DispensaryCard/DispensaryCard.js
+++ client/src/components/DispensaryCard/DispensaryCard.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { FcLike } from 'react-icons/fc';
+import { FcLike } from "react-icons/fc";
 // import { useStoreContext } from "../../utils/GlobalState";
-import { useAuth } from "../../contexts/AuthContext"
+import { useAuth } from "../../contexts/AuthContext";
+// import API from "../../utils/API";
 // import{ ADD_FAVORITE } from "../../utils/actions"
 
-function DispensaryCard({ name, location, display_phone, is_closed, rating, }) {
+function DispensaryCard({ name, location, display_phone, is_closed, rating }) {
   // const [state, dispatch] = useStoreContext();
-  const {currentUser} = useAuth();
+  const { currentUser } = useAuth();
 
+  //function handleFavorite...
+  // function handleInputChange(event) {
+  //   const { value } = event.target;
+  //   API.setFavorite(value);
+  // }
   // const addFavorite = () => {
   //   dispatch({
-  //     type: ADD_FAVORITE, 
+  //     type: ADD_FAVORITE,
   //     post: state.currentPost
   //   });
   // };
 
-
-
   return (
     <Card>
       <Card.Body>
-        <Card.Title style={{fontFamily:'Permanent Marker'}}>{name} 
-        {currentUser && <FcLike href="/favorites" ></FcLike>}
+        <Card.Title
+          // onClick={handleInputChange}
+          style={{ fontFamily: "Permanent Marker" }}
+        >
+          {name}
+          {currentUser && <FcLike href="/favorites"></FcLike>}
         </Card.Title>
         <Card.Text>
           {location.display_address.join(", ")} {display_phone}
