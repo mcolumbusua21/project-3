222cce2164964d6ede25fe8cd502072f3377c2d7
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-09 11:58:23 -0700

    clean up favpage, added style, started filter dups

--- client/src/pages/FavoritesPage/FavoritesPage.js
+++ client/src/pages/FavoritesPage/FavoritesPage.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar";
+import "./FavoritesPage.css";
+import { saveFavorite } from "../../utils/API";
+import { ListGroup } from "react-bootstrap";
+import Hero from "../../components/Hero";
 // import DeleteBtn from "../components/DeleteBtn";
 // import { REMOVE_FAVORITE, LOADING } from "../../utils/actions";
 // import List from "../../components/List"
 // import { Link } from "react-router-dom"
-// import { useStoreContext } from "../../utils/GlobalState";
-import "./FavoritesPage.css";
-import { saveFavorite } from "../../utils/API";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 const FavoritesPage = () => {
-  // const [state, dispatch] = useStoreContext();
   const [favorite, setFavorite] = useState([]);
 
   useEffect(() => {
-    // console.log(saveFavorite());
     const getFavorite = async () => {
       const listFavorite = await saveFavorite();
       setFavorite(listFavorite.data[0].favorites);
@@ -21,9 +20,6 @@ const FavoritesPage = () => {
     getFavorite();
   }, []);
   console.log(favorite);
-  // const getFavorites = () => {
-  //   dispatch({ type: LOADING });
-  // };
 
   // const removeFavorites = id => {
   //     dispatch({
@@ -34,24 +30,35 @@ const FavoritesPage = () => {
 
   return (
     <div className="container mb-5 mt-5">
-      <h1 className="text-center">Here's Your Favorite Dispensaries</h1>
+      <Navbar />
+      <Hero>
+        <h1 className="text-center">Here's Your Favorite Dispensaries</h1>
+      </Hero>
       <ListGroup>
         <div className="favorite-list">
-          {favorite.sort((a, b)=>{
-            if(a.name[0] < b.name[0]) 
-            {return -1;}
-            if(a.name[0] > b.name[0]){return 1;}
-            return 0;
-          }).map((favoriteIndex) => (
-            <div key={favoriteIndex.id}>
-              <h1 style={{ color: "black", backgroundColor: "yellow" }}>
-              {favoriteIndex.name}, {favoriteIndex.display_address},
-                {favoriteIndex.display_phone}, {favoriteIndex.is_open_},
-                {favoriteIndex.rating}
-              </h1>
-              {/* <ListGroupItem>{favoriteIndex}</ListGroupItem> */}
-            </div>
-          ))}
+          {favorite
+            .sort((a, b) => {
+              if (a.name[0] < b.name[0]) {
+                return -1;
+              }
+              if (a.name[0] > b.name[0]) {
+                return 1;
+              }
+              return 0;
+            })
+            .map((favoriteIndex) => (
+              <div key={favoriteIndex.id}>
+                <h1 style={{ color: "black", backgroundColor: "yellow" }}>
+                  {favoriteIndex.name}, {favoriteIndex.display_address},
+                  {favoriteIndex.display_phone}, {favoriteIndex.is_open_},
+                  {favoriteIndex.rating}
+                </h1>
+              </div>
+            ))
+            // .filter((favoriteName) =>{
+            //   return favoriteName.indexOf(favorite.name)
+            // })
+            }
         </div>
       </ListGroup>
     </div>
